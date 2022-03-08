d99049cfcf2447bc69592ee850bb17b4fc6b9a62
FravinCustodianStudent <91590998+FravinCustodianStudent@users.noreply.github.com>
2022-03-08 18:45:53 +0200

    modal update

--- js/script.js
+++ js/script.js
@@ -41,7 +41,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
     // Timer
 
-    const deadline = '2022-05-11';
+    const deadline = '2022-05-20';
 
     function getTimeRemaining(endtime) {
         const t = Date.parse(endtime) - Date.parse(new Date()),
